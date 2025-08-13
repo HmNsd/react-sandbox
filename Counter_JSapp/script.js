9df7cbaf9da96b3426df79c7277f0bde7574039d
Himanshu Nishad <hmnsd@Himanshus-MacBook-Air.local>
2025-08-13 23:25:43 +0530

    Counter JS app

---
+++ Counter_JSapp/script.js
@@ -0,0 +1,27 @@
+let count = 0;
+
+const countEl = document.getElementById("count");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+
+increaseBtn.addEventListener("click", () => {
+  count++;
+  updateDisplay();
+});
+
+decreaseBtn.addEventListener("click", () => {
+  if (count > 0) {
+    count--;
+    updateDisplay();
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  updateDisplay();
+});
+
+function updateDisplay() {
+  countEl.textContent = count;
+}
